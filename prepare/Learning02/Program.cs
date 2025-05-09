b45aec704c96d7a8a2d4997984d31a343cd5d1ec
Anderson-Packer <apacker7.23@gmail.com>
2025-05-08 19:40:36 -0600

    Prep 3

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+    Job job1 = new Job();
+
+    job1._jobTitle = "software engineer";
+    job1._company = "Microsft";
+    job1._startYear = "1970";
+    job1._endYear = "2001";
+
+    Job job2 = new Job();
+
+    job2._jobTitle = "coffee boy";
+    job2._company = "Apple";
+    job2._startYear = "2003";
+    job2._endYear = "2005";
+
+
+    Resume myResume = new Resume();
+    
+    myResume._name = "Anderson Packer";
+    myResume._jobs.Add(job1);
+    myResume._jobs.Add(job2);
+
+    myResume.Display();
     }
 }
